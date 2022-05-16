6f0baed5fa112ed2c9d0bfee145870fe85a17cf5
LongChang Ma <chad.ma@rock-chips.com>
2022-05-16 11:34:53 +0800

    clean and tidy rkupdate code with astyle.
    Signed-off-by: LongChang Ma <chad.ma@rock-chips.com>
    Change-Id: Id664fd22586aabe99577dce3848accbe2487264c

--- main.cpp
+++ main.cpp
@@ -5,33 +5,41 @@
 
 enum { INSTALL_SUCCESS, INSTALL_ERROR, INSTALL_CORRUPT };
 
-FILE* cmd_pipe = NULL;
+FILE *cmd_pipe = NULL;
 int sdBootUpdate = 0;
 
 
 void handle_upgrade_callback(char *szPrompt)
 {
-    if(cmd_pipe != NULL){
+    if (cmd_pipe != NULL)
+    {
         fprintf(cmd_pipe, "ui_print %s\n", szPrompt);
     }
 }
 
 void handle_upgrade_progress_callback(float portion, float seconds)
 {
-    if(cmd_pipe != NULL){
-        if (seconds==0)
+    if (cmd_pipe != NULL)
+    {
+        if (seconds == 0)
+        {
             fprintf(cmd_pipe, "set_progress %f\n", portion);
+        }
         else
+        {
             fprintf(cmd_pipe, "progress %f %d\n", portion, seconds);
+        }
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int status;
 
     setbuf(stdout, NULL);
     setbuf(stderr, NULL);
-    if(argc != 5) {
+    if (argc != 5)
+    {
         printf("unexpected number of arguments (%d)\n", argc);
         fprintf(stderr, "unexpected number of arguments (%d)\n", argc);
         return 1;
@@ -40,17 +48,21 @@ int main(int argc, char *argv[]) {
     cmd_pipe = fdopen(fd, "wb");
     setlinebuf(cmd_pipe);
 
-    char* filepath = argv[3];
+    char *filepath = argv[3];
     sdBootUpdate = atoi(argv[4]);
 
     //call update
     bool bRet = do_rk_firmware_upgrade(filepath, (void *)handle_upgrade_callback,
-                                        (void *)handle_upgrade_progress_callback);
+                                       (void *)handle_upgrade_progress_callback);
 
-    if(!bRet)
+    if (!bRet)
+    {
         status = INSTALL_ERROR;
+    }
     else
+    {
         status = INSTALL_SUCCESS;
+    }
 
     sleep(5);
     sync();
